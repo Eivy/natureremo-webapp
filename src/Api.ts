53878dd9216d84e0056394f629c770c3c8dc0f54
Eivy <modern.times.rock.and.roll+git@gmail.com>
2020-02-06 17:09:17 +0900

    Fix api
    url.resolve is good for join path for url

--- src/Api.ts
+++ src/Api.ts
@@ -1,25 +1,26 @@
 /// <reference path="schema.d.ts" />
 import request from 'request-promise-native'
-import * as path from 'path'
+import * as url from 'url'
 
 class Api {
   private basePath: string
-  private requestHeaders: Map<string, string>
+  private requestHeaders: any
 
-  constructor(token: string, server = "https://api.nature.global/1") {
+  constructor(token: string, server = "https://api.nature.global/1/") {
     this.basePath = server
-    this.requestHeaders = new Map<string, string>([['Authorization', 'Bearer ' + token]])
+    this.requestHeaders = { 'Authorization': 'Bearer ' + token }
   }
 
   async GetMe(): Promise<RemoAPI.User | null> {
-    return await request.get(path.join(this.basePath, 'users', 'me'), { headers: this.requestHeaders }).catch((reason) => {
-      console.log(reason)
-      return null
-    })
+    return await request.get(url.resolve(this.basePath, 'users/me'), { headers: this.requestHeaders })
+      .catch((reason) => {
+        console.log(reason)
+        return null
+      })
   }
 
   async PostMe(nickname: string): Promise<RemoAPI.User | null> {
-    return await request.post(path.join(this.basePath, 'users', 'me'),
+    return await request.post(url.resolve(this.basePath, 'users/me'),
       {
         headers: this.requestHeaders,
         form: { nickname: nickname }
