28fa2c00c606abd9cc6a4a9427ca8a301a03d8c2
Eivy <modern.times.rock.and.roll+git@gmail.com>
2020-02-07 17:36:45 +0900

    Add "Config" Component

---
+++ src/Config.test.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import { render } from '@testing-library/react'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Config from './Config';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+beforeEach(() => {
+  localStorage.clear()
+})
+
+test('renders config', () => {
+  const { getByText } = render(<Config />)
+  expect(getByText(/access token/i)).toBeInTheDocument()
+});
+
+test('default input value', () => {
+  localStorage.setItem('access_token', 'test')
+  const config = Enzyme.shallow(<Config />)
+  expect(config.find('input').get(0).props.value).toEqual('test')
+});
+
+test('save token to localStorage', () => {
+  const inputValue = 'saved to localStorage'
+  const config = Enzyme.shallow(<Config />)
+  const input = config.find('input').at(0)
+  const button = config.find('button').at(0)
+  input.simulate('change', { target: { value: inputValue } })
+  button.simulate('click')
+  expect(localStorage.getItem('access_token')).toBe(inputValue)
+})
---
+++ src/Config.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import './Config.scss'
+
+interface Props {
+}
+
+interface State {
+  token?: string,
+}
+
+class Config extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      token: localStorage.getItem("access_token") as string || ''
+    }
+    this.onTokenChange = this.onTokenChange.bind(this)
+  }
+
+  onTokenChange(event: any) {
+    localStorage.setItem('access_token', event.target.value)
+    this.setState({ token: event.target.value })
+  }
+
+  render() {
+    let token = this.state.token
+    return (
+      <div className="config">
+        <label>Access Token
+        <input value={token} onChange={this.onTokenChange} />
+        </label>
+        <button>Save</button>
+      </div>
+    )
+  }
+
+}
+
+export default Config
