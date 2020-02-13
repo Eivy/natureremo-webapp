57b465e9c9746eb9d3f9ede36318a04851001cfa
Eivy <modern.times.rock.and.roll+git@gmail.com>
2020-02-13 22:04:33 +0900

    Change Appliance Props

--- src/Appliance.test.tsx
+++ src/Appliance.test.tsx
@@ -6,7 +6,17 @@ import Adapter from 'enzyme-adapter-react-16'
 Enzyme.configure({ adapter: new Adapter() })
 
 test('renders ico_light', () => {
-  const { getByText } = render(<Appliance icon="ico_light" type="LIGHT" nickname="light" />);
+  const appliance: RemoAPI.Appliance = {
+    nickname: "light",
+    type: "LIGHT",
+    image: "ico_light",
+    light: {
+      state: {
+        power: "on"
+      }
+    }
+  }
+  const { getByText } = render(<Appliance data={appliance}/>);
   const altText= getByText('Light');
   expect(altText).toBeInTheDocument();
   const label = getByText('light');
@@ -14,7 +24,17 @@ test('renders ico_light', () => {
 });
 
 test('renders ico_etc', () => {
-  const { getByText } = render(<Appliance icon="ico_etc" type="IR" nickname="machine" />);
+  const appliance: RemoAPI.Appliance = {
+    nickname: "machine",
+    type: "IR",
+    image: "ico_etc",
+    light: {
+      state: {
+        power: "on"
+      }
+    }
+  }
+  const { getByText } = render(<Appliance data={appliance} />);
   const altText= getByText('Etc');
   expect(altText).toBeInTheDocument();
   const label = getByText('machine');
@@ -22,7 +42,17 @@ test('renders ico_etc', () => {
 });
 
 test('appliance which power', () => {
-  const appliance = Enzyme.shallow(<Appliance icon="ico_light" type="LIGHT" nickname="light" status="off" />);
+  const data: RemoAPI.Appliance = {
+    nickname: "light",
+    type: "LIGHT",
+    image: "ico_light",
+    light: {
+      state: {
+        power: "off"
+      }
+    }
+  }
+  const appliance = Enzyme.shallow(<Appliance data={data} />);
   let circle = appliance.find(".circle");
   expect(circle.at(0)).not.toBeNull();
   circle = appliance.find(".circle.on");
@@ -30,7 +60,17 @@ test('appliance which power', () => {
 });
 
 test('appliance which power on', () => {
-  const appliance = Enzyme.shallow(<Appliance icon="ico_light" type="LIGHT" nickname="light" status="on" />);
+  const data: RemoAPI.Appliance = {
+    nickname: "light",
+    type: "LIGHT",
+    image: "ico_light",
+    light: {
+      state: {
+        power: "on"
+      }
+    }
+  }
+  const appliance = Enzyme.shallow(<Appliance data={data} />);
   const circle = appliance.find(".circle.on").at(0);
   expect(circle).not.toBeNull();
 });
--- src/Appliance.tsx
+++ src/Appliance.tsx
@@ -20,10 +20,7 @@ const components: any = {
 }
 
 interface Props {
-  icon: string,
-  nickname: string,
-  type: string,
-  status?: string,
+  data: RemoAPI.Appliance,
 }
 
 interface State {
@@ -36,22 +33,24 @@ class Appliance extends React.Component<Props, State> {
     let Icon
     let mainClassName = ["main"]
     try {
-      Icon = components.hasOwnProperty(this.props.icon) ? components[this.props.icon] : Icons.Etc
+      Icon = components.hasOwnProperty(this.props.data.image) ? components[this.props.data.image!] : Icons.Etc
     }
     catch {
       Icon = Icons.Etc
     }
-    if (this.props.type === 'LIGHT' || this.props.type === 'AC') {
+    if (this.props.data.type === 'LIGHT') {
       power = (<button className="power"><Icons.Power /></button>)
       mainClassName.push("circle")
-      if (this.props.status === "on") {
-        mainClassName.push("on")
-      }
+      mainClassName.push(this.props.data.light!.state!.power!)
+    } else if (this.props.data.type === 'AC') {
+      power = (<button className="power"><Icons.Power /></button>)
+      mainClassName.push("circle")
+      mainClassName.push(this.props.data.settings!.button!.replace(/power-/, ''))
     }
     return (
         <div className="appliance">
         <button className={mainClassName.join(" ")}><Icon /></button>
-        <span className="label">{this.props.nickname}</span>
+        <span className="label">{this.props.data.nickname}</span>
         {power}
         </div>
         )
