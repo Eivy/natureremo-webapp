04bc594062fb5cba01b3dda1e0a25598e011d637
Eivy <modern.times.rock.and.roll+git@gmail.com>
2020-02-17 21:45:22 +0900

    Fix image values for Icon

--- src/Appliance.tsx
+++ src/Appliance.tsx
@@ -8,7 +8,7 @@ import * as Icons from './Icons'
 const components: any = {
   "ico_av": Icons.AV,
   "ico_ac_1": Icons.AirCon1,
-  "ico_ac_2": Icons.AirCon2,
+  "ico_ac_0": Icons.AirCon2,
   "ico_air_purifier": Icons.AirPurifier,
   "ico_audio": Icons.Audio,
   "ico_curtain": Icons.Curtain,
@@ -16,7 +16,7 @@ const components: any = {
   "ico_fan": Icons.Fan,
   "ico_light": Icons.Light,
   "ico_tv": Icons.TV,
-  "ico_vacuum": Icons.Vacuum,
+  "ico_robot": Icons.Vacuum,
 }
 
 interface Props {
@@ -39,11 +39,11 @@ class Appliance extends React.Component<Props, State> {
       Icon = Icons.Etc
     }
     if (this.props.data.type === 'LIGHT') {
-      power = (<button className="power"><Icons.Power /></button>)
+      power = (<button className="power"><Icons.Power power={this.props.data.light!.state!.power!} /></button>)
       mainClassName.push("circle")
       mainClassName.push(this.props.data.light!.state!.power!)
     } else if (this.props.data.type === 'AC') {
-      power = (<button className="power"><Icons.Power /></button>)
+      power = (<button className="power"><Icons.Power power={this.props.data.settings!.button!.replace(/power-/, '') === 'on' ? 'on' : 'off'} /></button>)
       mainClassName.push("circle")
       mainClassName.push(this.props.data.settings!.button!.replace(/power-/, ''))
     }
