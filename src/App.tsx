f00ebcaba809b31816dbe4684f93d0ed7d0700ce
Eivy <modern.times.rock.and.roll+git@gmail.com>
2020-02-21 17:47:11 +0900

    Add global header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.scss';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Top from './pages/Top';
 import Config from './pages/Config';
+import {Gear} from './components/Icons';
+import logo from './logo.svg';
 
 const App: React.FC = () => {
   return (
-  <Router>
-    <Switch>
-      <Route path="/config" component={Config} />
-      <Route path="/" component={Top} />
-    </Switch>
-  </Router>
+    <Router>
+      <header>
+        <Link to="/" className="logo"><img src={logo} alt="logo"/>RemoWebApp</Link>
+        <Link to="/config" className="config"><Gear/></Link>
+      </header>
+      <div id="main">
+        <Switch>
+          <Route path="/config" component={Config} />
+          <Route path="/" component={Top} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
