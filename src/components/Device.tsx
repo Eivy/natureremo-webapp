8b95f853ad7045c543c906c397b57ec2a7ff91ee
Eivy <modern.times.rock.and.roll+git@gmail.com>
2020-02-21 14:22:10 +0900

    Fix style of Device

--- src/components/Device.tsx
+++ src/components/Device.tsx
@@ -13,8 +13,8 @@ const Device: React.FC<Props> = React.memo((props) => {
         <header>
           <span className={styles.nickname}>{props.device.name}</span>
           <span className={styles.sensor}>
-            {props.device.newest_events?.te && <span>温度:<span className={styles.value}>{props.device.newest_events!.te!.val}</span>℃</span>}
-            {props.device.newest_events?.hu && <span className={styles.humid}>湿度:<span className={styles.value}>{props.device.newest_events!.hu!.val}</span>%</span>}
+            {props.device.newest_events?.te && <span>温度:<span className={styles.value}>{props.device.newest_events!.te!.val}℃</span></span>}
+            {props.device.newest_events?.hu && <span className={styles.humid}>湿度:<span className={styles.value}>{props.device.newest_events!.hu!.val}%</span></span>}
             {props.device.newest_events?.il && <span className={styles.illumi}>明るさ:<span className={styles.value}>{props.device.newest_events!.il!.val}</span></span>}
           </span>
         </header>
