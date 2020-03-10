ccdd07aa07c781aef87cc09da3710db744de48b0
Eivy <modern.times.rock.and.roll+git@gmail.com>
2020-03-10 17:51:59 +0900

    Add test for app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Api from './Api';
 import App from './App';
+import store from './stores';
 
-test('renders learn react link', () => {
+describe('app', () => {
+
+  const mockDevices = jest.spyOn(Api, 'GetDevices');
+  const mockAppliances = jest.spyOn(Api, 'GetAppliances');
+
+  beforeEach(() => {
+    mockDevices.mockReset();
+    mockAppliances.mockReset();
+    mockDevices.mockResolvedValue(new Promise((resolve, reject) => {
+      resolve([{"id": "device_id", "name": "test_device", "newest_events": {}}]);
+    }));
+    mockAppliances.mockResolvedValue(new Promise((resolve, reject) => {
+      resolve([{"id": "appliance_id", "nickname": "test_appliance", "device": {"id": "device_id"}},{"id": "appliance_id_not_render", "nickname": "test_appliance_not_render", "device": {"id": "device_id_not_contain"}}]);
+    }));
+  });
+
+  test('render app', () => {
+    localStorage.setItem('access_token', 'test');
+    var app = render(<Provider store={store}><App /></Provider>);
+    setImmediate(() => {
+      expect(app.getByText('Gear')).toBeInTheDocument();
+    });
+  });
+
 });
